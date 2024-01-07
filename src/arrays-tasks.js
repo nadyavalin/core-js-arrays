47bd0f6003bd2613be6357866357a6e83a54bdce
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-01-07 21:45:44 +0300

    feat: make the function shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -540,8 +540,20 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  if (n === 0) {
+    return arr;
+  }
+  if (n > 0) {
+    const rightShift = arr.slice(-n);
+    arr.splice(-n, n);
+    Array.prototype.unshift.apply(arr, rightShift);
+  } else {
+    const leftshift = arr.slice(0, -n);
+    arr.splice(0, -n);
+    Array.prototype.push.apply(arr, leftshift);
+  }
+  return arr;
 }
 
 /**
